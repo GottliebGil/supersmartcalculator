2e83f76e35b63863ff48a79c22ba313ba8a84bfb
Gil Gottlieb <gilgottlieb1@gmail.com>
2017-09-03 22:55:22 +0300

    fix parentheses

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import math
 actions = {
     '+': 1,
     '-': 1,
@@ -10,29 +11,36 @@ actions = {
     '$': 5,
     '&': 5
 }
-
+one_parameter_actions = ['!']
 
 def perform_action(val):
+    """Receives an action type (two parts and an action) and performs an action on it"""
     if val['a'] == '' and val['action'] == '-':
         val['action'] = ''
         val['b'] = -14
         return val['b']
-    val['a'] = float(val['a'])
-    val['b'] = float(val['b'])
+    if val['a'] != '':
+        val['a'] = float(val['a'])
+    if val['b'] != '':
+        val['b'] = float(val['b'])
     a = val['a']
     b = val['b']
-    return {
-        '+': a + b,
-        '-': a - b,
-        '*': a * b,
-        '/': a / b,
-        '^': a ** b,
-        '%': a % b,
-        '!': a,  # TODO
-        '@': (a + b) / 2,
-        '$': max(a, b),
-        '&': min(a, b)
-    }[val['action']]
+    if val['b'] == '':
+        return {
+            '!': math.factorial(a)  # TODO
+        }[val['action']]
+    else:
+        return {
+            '+': a + b,
+            '-': a - b,
+            '*': a * b,
+            '/': a / b,
+            '^': a ** b,
+            '%': a % b,
+            '@': (a + b) / 2,
+            '$': max(a, b),
+            '&': min(a, b)
+        }[val['action']]
 
 
 def delete_spaces(val):
@@ -53,18 +61,29 @@ def contains(val, char):
 def strongest_action(val):
     """Returns the strongest action in a string"""
     current = -1
-    for action in actions:
-        if contains(val, action) and current == -1:
-            current = action
-        elif contains(val, action) and actions[action] > actions[current]:
-            current = action
+    index = 0
+    for char in val:
+        if char in actions and current == -1 and index != 0:
+            current = char
+        elif char in actions and index != 0 and actions[char] > actions[current]:
+            current = char
+        index += 1
+    return current
+
+
+def actions_counter(val):
+    """Returns the number of actions left to calculate in value"""
+    current = 0
+    for char in val:
+        if char in actions:
+            current += 1
     return current
 
 
 def get_first_numbers(val):
     count_start = 0
     for char in val:
-        if char in actions:
+        if char in actions and count_start > 0:
             return val[:count_start]
         if count_start == len(val) - 1:
             return val
@@ -79,14 +98,14 @@ def get_next_action(val):
     }
     count_start = 0
     count_symbol = 0
-    
+
     if strongest_action(val) == -1:
-        to_return['b'] = val
+        to_return['b'] = val or ''
         to_return['action'] = ''
         to_return['a'] = ''
         return to_return
 
-    if val[0] in actions and strongest_action(val) == val[0]:
+    if val[0] in actions and actions_counter(val) == 1 and strongest_action(val) == val[0]:
         to_return['b'] = val[:len(val)]
         to_return['action'] = ''
         to_return['a'] = ''
@@ -94,12 +113,18 @@ def get_next_action(val):
     for char in val:
         count_start += 1
         count_symbol += 1
-        if char == strongest_action(val):
-            to_return['a'] = val[count_start - count_symbol:count_start - 1]
-            to_return['b'] = get_first_numbers(val[count_start:])
+        if char == strongest_action(val) and char in one_parameter_actions:
+            to_return['a'] = val[count_start - count_symbol:count_start - 1] or ''
+            to_return['b'] = ''
             to_return['action'] = val[count_start - 1:count_start]
             return to_return
-        elif char in actions:
+
+        if char == strongest_action(val) and count_start != 1:
+            to_return['a'] = val[count_start - count_symbol:count_start - 1] or ''
+            to_return['b'] = get_first_numbers(val[count_start:]) or ''
+            to_return['action'] = val[count_start - 1:count_start] or ''
+            return to_return
+        elif char in actions and count_start != 1:
             count_symbol = 0
 
 
@@ -109,6 +134,12 @@ def replace_action(val, action):
 
 
 def calculate_block(val):
+    # Firstly, I'll calculate all the parentheses
+    block = get_block(val)
+    while block != '':
+        calculated = calculate_block(block)
+        val = val.replace('(' + block + ')', calculated)
+        block = get_block(val)
     next = get_next_action(val)
     while next['action'] != '':
         print 'next action is %s ' % next
@@ -118,9 +149,24 @@ def calculate_block(val):
         print '-------------'
     return next['b']
 
+
+def get_block(val):
+    indent = 0
+    block = ''
+    for char in val:
+        if char == ')':
+            indent += -1
+        if indent == 0 and block != '':
+            return block
+        if indent > 0:
+            block += char
+        if char == '(':
+            indent += 1
+    return ''
+
+
 def main():
     val = raw_input("Enter ")
-    # val = '5+4-18'
     val = delete_spaces(val)
     result = calculate_block(val)
     print 'result is %s ' % result
